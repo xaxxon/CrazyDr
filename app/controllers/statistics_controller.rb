3deff0aa58adaf9b6e24f5ae8167826580ae5c0c
Zac Hansen <zhansen@zac-hansens-macbook.local>
2009-10-17 04:14:02 -0700

    added files corresponding to new views/contorllers and stylesheets

---
+++ app/controllers/statistics_controller.rb
@@ -0,0 +1,23 @@
+class StatisticsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+end
