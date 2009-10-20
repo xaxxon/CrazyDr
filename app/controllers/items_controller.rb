ae287995e85f8b5e508f597563b3ebef5ef725ac
Zac Hansen <zhansen@173-116-155-184.pools.spcsdns.net>
2009-10-19 18:42:36 -0700

    removed a bunch of unused things and updated some stuff

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.find( :all )
-    @recipes = Recipe.find( :all )
     @new_item = Item.new
-    @recipe = Recipe.new
   end
 
   def show
